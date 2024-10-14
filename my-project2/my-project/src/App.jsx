7a7b603c969e6fa9b5297d6ba21002dba6cec559
Wintorez7 <liordx76@gmail.com>
2024-10-14 21:40:29 +0530

    RGB coded add

--- my-project2/my-project/src/App.jsx
+++ my-project2/my-project/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Task1 from './component/Task1'
+import Task2 from './component/Task2'
 
 function App() {
  
 
   return (
    <div className='w-full h-screen '>
-      <Task1/>
+      {/* <Task1/> */}
+      <Task2/>
    </div>
   )
 }
